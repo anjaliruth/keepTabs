7eae4a0fb9f15dde7413aecc33480c810edca950
anjaliruth <ruthanjali0211@gmail.com>
2023-08-20 10:12:47 +0100

    changed title

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Inventory</h1>
+        <h1>Whereabouts Wizards</h1>
         <div className="totalDisplayShift">
         <List/>
         </div>
